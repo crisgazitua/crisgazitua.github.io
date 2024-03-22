aa3fd4e7bcacbc19a345776927b51ef1f8c7eaab
crisgazitua <cggazitua@miuandes.cl>
2024-03-22 15:24:37 -0300

    lab2.2

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+document.addEventListener("DOMContentLoaded",() => {
+    alert("Welcome to my personal page!");
+})
+
 function cambiarTexto() {
     var nuevoTexto = document.getElementById("inputText").value;
     document.getElementById("seccionCambiante").innerText = nuevoTexto;
 }
-
-var img=document.createElement('img');
-img.src='IMG_0801.jpg';
-document.body.appendChild(img);
 
